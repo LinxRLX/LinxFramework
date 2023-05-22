9c5094a4a95dd735a9b895bc5a5def000b403bc5
ruanlinxi <2208534778@qq.com>
2023-05-22 21:02:22 +0800

    使用性能更好的实现方式

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class Player : MonoBehaviour
 {
@@ -79,18 +81,36 @@ public class Player : MonoBehaviour
         // 设置角色转向
         if (m_target != null)
         {
-            var orginForward = transform.forward;
-            transform.LookAt(m_target.position, Vector3.up);
-            var targetForward = transform.forward;
-            targetForward.y = 0;
-            transform.forward = Vector3.Slerp(orginForward, targetForward, Time.deltaTime * targetRotateSpeed);
+            // System.Diagnostics.Stopwatch stopwatch = new Stopwatch();
+            // stopwatch.Start(); //  开始监视代码运行时间
+            // 方式1--欧拉角  速度0.005
+            Vector3 targetDir = m_target.transform.position - transform.position;
+            targetDir.y = 0;
+            var targetRotation = Quaternion.LookRotation(targetDir);
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * targetRotateSpeed);
+
+            // // 方式2--lookat 速度 0.007+
+            // var orginForward = transform.forward;
+            // transform.LookAt(m_target.position, Vector3.up);
+            // var targetForward = transform.forward;
+            // targetForward.y = 0;
+            // transform.forward = Vector3.Slerp(orginForward, targetForward, Time.deltaTime * targetRotateSpeed);
+
+            // // 方式3--RotateTowards 速度0.005
+            // // Vector3.Angle(transform.forward, m_target.transform.position - transform.position);
+            // Vector3 dir = m_target.transform.position - transform.position;
+            // dir.y = 0;
+            // transform.forward =
+            //     Vector3.RotateTowards(transform.transform.forward, dir, targetRotateSpeed * Time.deltaTime, 0);
+
+            // stopwatch.Stop(); //  停止监视
+            // TimeSpan timespan = stopwatch.Elapsed;
+            // Debug.Log("Cost:"+timespan.TotalMilliseconds);
         }
-        else
+        else if (IsWalking && transform.forward != moveDir)
         {
-            if (transform.forward != moveDir)
-            {
-                transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-            }
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
     }
 
